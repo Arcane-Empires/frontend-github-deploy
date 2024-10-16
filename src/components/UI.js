f720c64b9be004b26fe2035fdca92859a62a8fb8
drinkincode] <jonnysmith696910@gmail.com>
2024-10-16 01:39:25 -0500

    bugfix for cors error

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import apiCall  from '../services/apiCall'
 
 const UI = () => {
+  const fetchData = async () => {
+    try {
+      const data = await apiCall('https://node-server-379840034411.us-central1.run.app/api/user');
+      console.log('Data:', data);
+      return data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
   return (
     <div>
       <h1>Game UI</h1>
       {/* Add UI elements here */}
+      <button onClick={fetchData}>Fetch Data</button>
     </div>
   );
 };
